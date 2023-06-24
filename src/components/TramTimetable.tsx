e659a90b6876122d2df2c43a83eb9d18c374a460
jacretney <jacretney@gmail.com>
2023-06-24 17:43:21 +0100

    Better polling logic

--- src/components/TramTimetable.tsx
+++ src/components/TramTimetable.tsx
@@ -1,24 +1,37 @@
 import TfgmClient from "../data/TfgmApi/TfgmClient";
 import TfgmAxiosClient from '../data/TfgmApi/TfgmAxiosClient';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function TramTimetable() {
     const client = new TfgmClient(TfgmAxiosClient);
 
-    const [response, setResponse] = useState({});
-  
-    setInterval(() => {
-        const stop = client.fetchMetroLinkStopDetails(127967);
+    const [response, setResponse] = useState({
+        tram_0_destination: 'Loading...',
+        tram_0_arrival: '',
+        tram_1_destination: '',
+        tram_1_arrival: '',
+        message: '',
+    });
 
-        stop.then(data => setResponse({
-            'station': data.StationLocation,
-            'tram_0_destination' : data.Dest0,
-            'tram_0_arrival': `${data.Status0} in ${data.Wait0} mins.`,
-            'tram_1_destination' : data.Dest1,
-            'tram_1_arrival': `${data.Status1} in ${data.Wait1} mins.`,
-            'message': `${data.MessageBoard}`
-        }));
-    }, 15 * 1000);
+    const fetchData = () => {
+        client.fetchMetroLinkStopDetails(127967)
+            .then(data => setResponse({
+                tram_0_destination : data.Dest0,
+                tram_0_arrival: `${data.Status0} in ${data.Wait0} mins.`,
+                tram_1_destination : data.Dest1,
+                tram_1_arrival: `${data.Status1} in ${data.Wait1} mins.`,
+                message: `${data.MessageBoard}`
+            }));
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchData();
+        }, 5 * 1000);
+
+        return () => clearInterval(interval);
+    }, [client]);
+
   
     return (
         <div className="table">
